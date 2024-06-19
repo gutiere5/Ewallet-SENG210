32a7fe065fabdf1787b6779cd7795cbcd762cc87
PhelixBrooks <114290440+BrooksPFSU@users.noreply.github.com>
2024-06-18 20:52:25 -0400

    Update User.java

---
+++ src/User.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;  
+
+public class User {
+	private ArrayList <Currency>currencyRates = new ArrayList<Currency>();
+	private ArrayList <Wage>Income = new ArrayList<Wage>();  // user income sources that user can record or view or search by type or month 
+	private ArrayList <Expense>Spending = new ArrayList<Expense>(); //user's expenses 
+	String username;
+	String pwd;
+	//current total income - total 
+	double balance;
+	// possible monthly savings, calculated using monthly income (most recent) assuming the data we have is for one year, and monthly and biweekly expenses, here you can assume yearly expenses that are recorded have already been paid. 
+	double monthlysavings;	
+	//should add constructor(s)
+	User(String username,String password){
+		
+	}
+	
+	public void addExpenseList(Expense Ex) {
+		Spending.add(Ex);
+	}
+	
+	public void addIncomeList(Wage W) {
+		Income.add(W);
+	}
+}
