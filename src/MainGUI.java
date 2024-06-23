4b62024652e57fc96d5a2ef9045a003b93b410f7
Elber Gutierrez <elgu404@gmail.com>
2024-06-23 17:05:56 -0400

    Jackson GUI Updates
    (1) a button for loading a file
    (2) some way to get a filepath (text input is acceptable),
     (3) some way of choosing whether the file should be loaded as an
    expense or income file (having separate buttons for loading income or
    expense is acceptable)
    (4) a button for exporting a file,
    (5) a way to choose whether to export Expense, Income, ExpenseByType, or
    IncomeByType (a JList or checkbox-system would work), and
     (6) a text input to determine which type of income or expense will be
    exported.

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 
 public class MainGUI extends JFrame{
 
@@ -10,6 +13,9 @@ public class MainGUI extends JFrame{
 
 	// Initialize JPanel cards as a field
 	JPanel cards;
+	
+	// TODO TESTING THIS FILE CHOOSER
+	JFileChooser importFile = new JFileChooser("C:\\",FileSystemView.getFileSystemView());
 	
 	// JTextFields
 	JTextField usernameField = new JTextField();		// Used in loginWindow()
@@ -20,25 +26,35 @@ public class MainGUI extends JFrame{
 	JTextField sourceExpenseField = new JTextField();	// Used in mainPanel()
 	JTextField amountExpenseField = new JTextField();	// Used in mainPanel()
 	JTextField yearlyFreqField = new JTextField();		// Used in mainPanel()
-	static JTextField filterField = new JTextField();	// Used in reportPanel();
-
+	static JTextField filterField = new JTextField();	// Used in reportPanel()
+	
 	// JButtons
 	JButton loginButton = new JButton("Login");									// Used in loginWindow()
 	JButton addExpenseButton = new JButton("Add Expense");						// Used in mainPanel()
 	JButton addIncomeButton = new JButton("Add Income");						// Used in mainPanel()
 	JButton reportWindowButton = new JButton("Reports");						// Used IN mainPanel()
 	JButton covertForeiCurrcyButton = new JButton("Convert Foreign Currency");	// Used in mainPanel()
+	JButton importIncomeFileButton	= new JButton("Import Income File");			// Used in mainPanel()
+	JButton importExpenseFileButton = new JButton("Import Expense File");				// Used in mainPanel()
 	JButton returnButton = new JButton("Return To Main Menu"); 					// Used in reportPanel()
 	JButton filterButton = new JButton("Filter");			 					// Used in reportPanel()
 	JButton printIncomeButton = new JButton("Income Report");					// Used in reportPanel()
 	JButton printExpenseButton = new JButton("Expense Report");					// Used in reportPanel()
 	JButton printFullReportButton = new JButton("Print Full Report");			// Used in reportPanel()
-
+	JButton exportFileButton = new JButton("Export Current Report");			// Used in reportPanel()
+	
 	// JLabels
 	JLabel statusLabel = new JLabel();									// Used in loginWindow() and to show who is logged in
 	JLabel reportTitle = new JLabel("Report", SwingConstants.CENTER);	// Used in reportWindow() and actionPerformed()
 	JLabel currencyLabel = new JLabel("Currency: ");					// Used in MainPanel()
-	
+
+	//JRadioButtons
+	JRadioButton expenseRadio = new JRadioButton("Expense");
+	JRadioButton incomeRadio = new JRadioButton("Income");
+	JRadioButton expenseTypeRadio = new JRadioButton("Expense By Type");
+	JRadioButton incomeTypeRadio = new JRadioButton("Expense By Type");
+	ButtonGroup exportButtonGroup = new ButtonGroup();
+		
 	
 	/**
 	 * Constructor for MainGUI
@@ -57,7 +73,12 @@ public class MainGUI extends JFrame{
 		
 		//TODO Initializes currency, for currency conversion testing
 		calc.addCurrency("TestCur", 2);
-		//currencyLabel.setText(getName(Ca));
+		
+		// Adding Components to Button Group
+		exportButtonGroup.add(expenseRadio);
+		exportButtonGroup.add(incomeRadio);
+		exportButtonGroup.add(expenseTypeRadio);
+		exportButtonGroup.add(incomeTypeRadio);
 
 		// Create card panel with Card Layout
 		cards = new JPanel(new CardLayout());
@@ -159,17 +180,21 @@ public class MainGUI extends JFrame{
 		gbc.gridy = 4;
 		centerPanel.add(addExpenseButton, gbc);
 		//////////////
-
+		
 		// Add buttons to actions panel
 		actionPanel.add(actionsLabel);
 		actionPanel.add(Box.createVerticalStrut(5)); // Add space between label and buttons
 		actionPanel.add(reportWindowButton);
 		actionPanel.add(Box.createVerticalStrut(5)); // Add space between buttons
 		actionPanel.add(covertForeiCurrcyButton);
-		actionPanel.add(Box.createVerticalStrut(10)); // Add space between buttons
+		actionPanel.add(Box.createVerticalStrut(5)); // Add space between buttons
+		actionPanel.add(importIncomeFileButton);
+		actionPanel.add(Box.createVerticalStrut(5)); // Add space between buttons
+		actionPanel.add(importExpenseFileButton);
+		actionPanel.add(Box.createVerticalStrut(20)); // Add space between buttons
 		actionPanel.add(currencyLabel);
 		actionPanel.add(savingsLabel);
-
+		
 		// Add the sub-panel to the center of the main panel
 		mainPanel.add(centerPanel, BorderLayout.CENTER);
 		mainPanel.add(actionPanel, BorderLayout.EAST);
@@ -228,18 +253,35 @@ public class MainGUI extends JFrame{
 		
 		// JLabels
 		JLabel filterLabel = new JLabel("Filter By Type");
-
+		
+		
 		
 		// Adding components to westReportPanel
+		westReportPanel.add(Box.createVerticalStrut(35)); // Add space between components
 		westReportPanel.add(filterLabel);
+		westReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
 		westReportPanel.add(filterField);
+		westReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
 		westReportPanel.add(filterButton);
+		westReportPanel.add(Box.createVerticalStrut(35)); // Add space between components
 		
 		// Adding components to eastReportPanel
 		eastReportPanel.add(printIncomeButton);
+		eastReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
 		eastReportPanel.add(printExpenseButton);
+		eastReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
 		eastReportPanel.add(printFullReportButton);
+		eastReportPanel.add(Box.createVerticalStrut(15)); // Add space between components
+		eastReportPanel.add(expenseRadio);
+		eastReportPanel.add(incomeRadio);
+		eastReportPanel.add(expenseTypeRadio);
+		eastReportPanel.add(incomeTypeRadio);		
+		eastReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
+		eastReportPanel.add(exportFileButton);
+		eastReportPanel.add(Box.createVerticalStrut(5)); // Add space between components
 
+		
+		
 		// Adding components to mainReportPanel
 		mainReportPanel.add(reportTitle, BorderLayout.NORTH);
 		mainReportPanel.add(reportList, BorderLayout.CENTER);
@@ -309,6 +351,7 @@ public class MainGUI extends JFrame{
 				
 				// Switches to the Report Panel 
 				cardLayout.show(cards, "ReportPanel");
+				setSize(700, 350);
 			}	
 		});
 
@@ -346,6 +389,7 @@ public class MainGUI extends JFrame{
 
 				// Switches to the Main Panel 
 				cardLayout.show(cards, "MainPanel");
+				setSize(700, 300);
 			}
 		});
 
@@ -366,8 +410,34 @@ public class MainGUI extends JFrame{
 
 			}
 		});
-	}
+		
+		
+		//TODO Import File Button Action
+		importIncomeFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Handle import File action here
+				
+				importFile.showSaveDialog(null);
+			}
+		});
+		
+		// TODO Expense File Button Action
+		importExpenseFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Handle expense File action here
+				importFile.showSaveDialog(null);
 
+			}
+		});
+		
+		// TODO Export Current File Button Action 
+		exportFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Handle Export File action here
+
+			}
+		});
+	}
 
 	public static void main(String[] args) {
 		MainGUI GUI = new MainGUI();
